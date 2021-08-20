4e5b11de91fb306e5341ddffd750604aa7643a15
Sergey2709 <svk197809@gmail.com>
2021-08-20 11:21:41 +0300

    20/08/21-2

--- lesson9/homework9/src/App.js
+++ lesson9/homework9/src/App.js
@@ -1,18 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-// import Button from './Button';
-import PlaceholderPost from './PlaceholderPost';
-import Comment from './Comment';
-import Comment2 from './Comment2';
-
 
 function App() {
   return (
-    <>
-      {/* <PlaceholderPost /> */}
-      {/* <Comment/> */}
-      <Comment2/>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
