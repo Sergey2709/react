23c40a9fa58396e9fe3fd31e59c620a7334f4d57
Sergey2709 <svk197809@gmail.com>
2021-08-27 17:53:48 +0300

    27/08/21-1

---
+++ lesson12/myhw12/src/App.js
@@ -0,0 +1,14 @@
+import GoodsList from "./container/GoodsList";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="goods-field">
+        <GoodsList />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ lesson12/myhw12/src/store/index.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+import goodsReducer from './goodsSlice';
+
+export default configureStore({
+    reducer: {
+        goods: goodsReducer,
+
+    }
+
+})
